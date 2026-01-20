11bf2f0e4a38e2ea2e01f5bb520ef13ce23869b1
@OJK0029 <karlo.mat19@gmail.com>
2026-01-20 10:52:54 +0100

    commit: tailwind setup

---
+++ app/onboadring.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const onboadring = () => {
+  return (
+    <View>
+      <Text>onboadring</Text>
+    </View>
+  )
+}
+
+export default onboadring
+
+const styles = StyleSheet.create({})
